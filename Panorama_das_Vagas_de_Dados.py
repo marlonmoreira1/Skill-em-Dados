4a60c384b04eeed23b65e8737a503f93a458cee4
Márlon Moreira <71144665+marlonmoreira1@users.noreply.github.com>
2024-04-01 09:27:52 -0300

    Update Panorama_das_Vagas_de_Dados.py

--- Panorama_das_Vagas_de_Dados.py
+++ Panorama_das_Vagas_de_Dados.py
@@ -264,7 +264,7 @@ with coluna3.container(border=True):
 
 with coluna4.container(border=True):    
 
-    make_bar_with_no_slice(soft_skills,'soft_skills','Soft Skills mais Demandas para Profissionais de Dados',14,11,20)
+    make_bar_with_no_slice(soft_skills,'soft_skills','Soft Skills mais Demandas para Profissionais de Dados',14,12,20)
 
 
 coluna5, coluna6 = st.columns((1,1))
