55f6b1a5e46365b78a0bdbbe986cd14dcda3da67
Márlon Moreira <71144665+marlonmoreira1@users.noreply.github.com>
2024-06-17 11:52:04 -0300

    Update Panorama_das_Vagas_de_Dados.py

--- Panorama_das_Vagas_de_Dados.py
+++ Panorama_das_Vagas_de_Dados.py
@@ -26,7 +26,7 @@ credentials_file = os.environ.get("bigquery")
 with open(credentials_file) as json_file:
     json_data = json.load(json_file)
 
-client = bigquery.Client(credentials=service_account.Credentials.from_service_account_info(json_data), project=json_data['project_id'])
+client = bigquery.Client(credentials=service_account.Credentials.from_service_account_info(credentials_file), project=credentials_file['project_id'])
 
 @st.cache_data(ttl=28800)
 def consultar_dados_bigquery(consulta):
