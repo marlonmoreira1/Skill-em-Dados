ec3c6068a8d1b592c0eaf4384fb2ee06386a28fe
Márlon Moreira <71144665+marlonmoreira1@users.noreply.github.com>
2024-06-17 14:32:32 -0300

    Update Panorama_das_Vagas_de_Dados.py

--- Panorama_das_Vagas_de_Dados.py
+++ Panorama_das_Vagas_de_Dados.py
@@ -24,7 +24,7 @@ st.markdown("""
 type = os.getenv('TYPE')
 project_id = os.getenv('PROJECT_ID')
 private_key_id = os.getenv('PRIVATE_KEY_ID')
-private_key = os.getenv('PRIVATE_KEY').replace('\\n', '\n')  # Corrigir formatação da chave privada se necessário
+private_key = os.getenv('PRIVATE_KEY')
 client_email = os.getenv('CLIENT_EMAIL')
 client_id = os.getenv('CLIENT_ID')
 auth_uri = os.getenv('AUTH_URI')
