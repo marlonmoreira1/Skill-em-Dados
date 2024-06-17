1a10198b77c692a5e3f373af8ea02228f0375a7e
Márlon Moreira <71144665+marlonmoreira1@users.noreply.github.com>
2024-06-17 19:19:13 -0300

    Update Perfil Ferramentas e Habilidades por Cargo.py

--- pages/Perfil Ferramentas e Habilidades por Cargo.py
+++ pages/Perfil Ferramentas e Habilidades por Cargo.py
@@ -19,13 +19,23 @@ st.markdown("""
         </style>
         """, unsafe_allow_html=True)
 
-credentials_file = ".github/workflows/dadossobredados-e5a357810ee8.json"
 
-with open(credentials_file) as json_file:
-    json_data = json.load(json_file)
+credentials = {
+  "type": st.secrets["TYPE"],
+  "project_id": st.secrets["PROJECT_ID"],
+  "private_key_id":st.secrets["PRIVATE_KEY_ID"],
+  "private_key": st.secrets["PRIVATE_KEY"],
+  "client_email": st.secrets["CLIENT_EMAIL"],
+  "client_id": st.secrets["CLIENT_ID"],
+  "auth_uri": st.secrets["AUTH_URI"],
+  "token_uri": st.secrets["TOKEN_URI"],
+  "auth_provider_x509_cert_url": st.secrets["AUTH_PROVIDER_X509_CERT_URL"],
+  "client_x509_cert_url": st.secrets["CLIENT_X509_CERT_URL"],
+  "universe_domain": st.secrets["UNIVERSE_DOMAIN"]
+}
 
 
-client = bigquery.Client(credentials=service_account.Credentials.from_service_account_info(json_data), project=json_data['project_id'])
+client = bigquery.Client(credentials=service_account.Credentials.from_service_account_info(credentials), project=credentials['project_id'])
 
 
 @st.cache_data(ttl=28800)
