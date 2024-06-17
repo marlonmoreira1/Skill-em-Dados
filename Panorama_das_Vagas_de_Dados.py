8c3ec545ffbe90cb5dadd9ed1237d8ab3d8756bb
Márlon Moreira <71144665+marlonmoreira1@users.noreply.github.com>
2024-06-17 14:31:59 -0300

    Update Panorama_das_Vagas_de_Dados.py

--- Panorama_das_Vagas_de_Dados.py
+++ Panorama_das_Vagas_de_Dados.py
@@ -21,8 +21,32 @@ st.markdown("""
         </style>
         """, unsafe_allow_html=True)
 
-credentials_file = ${{BIGQUERY}}
-st.text(credentials_file)
+type = os.getenv('TYPE')
+project_id = os.getenv('PROJECT_ID')
+private_key_id = os.getenv('PRIVATE_KEY_ID')
+private_key = os.getenv('PRIVATE_KEY').replace('\\n', '\n')  # Corrigir formatação da chave privada se necessário
+client_email = os.getenv('CLIENT_EMAIL')
+client_id = os.getenv('CLIENT_ID')
+auth_uri = os.getenv('AUTH_URI')
+token_uri = os.getenv('TOKEN_URI')
+auth_provider_x509_cert_url = os.getenv('AUTH_PROVIDER_X509_CERT_URL')
+client_x509_cert_url = os.getenv('CLIENT_X509_CERT_URL')
+universe_domain = os.getenv('UNIVERSE_DOMAIN')
+
+credentials_file = {
+    "type": type,
+    "project_id": project_id,
+    "private_key_id": private_key_id,
+    "private_key": private_key,
+    "client_email": client_email,
+    "client_id": client_id,
+    "auth_uri": auth_uri,
+    "token_uri": token_uri,
+    "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+    "client_x509_cert_url": client_x509_cert_url,
+    "universe_domain": universe_domain
+}
+
 #json_data = json.loads(credentials_file)
 
 if credentials_file is None:
