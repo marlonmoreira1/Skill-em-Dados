5eeeab736ac108069491e4a274276600a3f9ae49
Márlon Moreira <71144665+marlonmoreira1@users.noreply.github.com>
2024-06-17 14:35:00 -0300

    Update Panorama_das_Vagas_de_Dados.py

--- Panorama_das_Vagas_de_Dados.py
+++ Panorama_das_Vagas_de_Dados.py
@@ -21,7 +21,7 @@ st.markdown("""
         </style>
         """, unsafe_allow_html=True)
 
-type = os.getenv('TYPE')
+type = os.environ["TYPE"]
 project_id = os.getenv('PROJECT_ID')
 private_key_id = os.getenv('PRIVATE_KEY_ID')
 private_key = os.getenv('PRIVATE_KEY')
