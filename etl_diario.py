d8b9f8508e9bd3c6b8b98fb9f0fc26f3fc1bc7d6
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-07-08 11:00:01 -0300

    Update etl_diario.py

--- etl_diario.py
+++ etl_diario.py
@@ -250,6 +250,20 @@ dataframe = jobs[['company_name', 'via', 'job_id', 'unique_key',
 
 dataframe['date'] = dataframe['date'].astype(str)
 
+credentials = {
+  "type": os.environ["TYPE"],
+  "project_id": os.environ["PROJECT_ID"],
+  "private_key_id": os.environ["PRIVATE_KEY_ID"],
+  "private_key": os.environ["PRIVATE_KEY"],
+  "client_email": os.environ["CLIENT_EMAIL"],
+  "client_id": os.environ["CLIENT_ID"],
+  "auth_uri": os.environ["AUTH_URI"],
+  "token_uri": os.environ["TOKEN_URI"],
+  "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
+  "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"],
+  "universe_domain": os.environ["UNIVERSE_DOMAIN"]
+}
+
 credentials_file = os.environ["BigQuery_CREDENTIALS"]
 
 with open(credentials_file) as json_file:
