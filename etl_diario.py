2a9c43edd16a156f6652db41159ef8d7a66b4bff
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-07-31 09:15:07 -0300

    Update etl_diario.py

--- etl_diario.py
+++ etl_diario.py
@@ -61,10 +61,10 @@ def get_dados(query,api_key):
         
     return google_jobs_results
 
-analista_dados = get_dados("analista de dados",analista_dados_key_api)
-analista_bi = get_dados("analista de bi",analista_bi_key_api)
-cientista_dados = get_dados("cientista de dados",cientista_dados_key_api)
-engenheiro_dados = get_dados("engenheiro de dados",engenheiro_dados_key_api)    
+analista_dados = get_dados("Analista Dados",analista_dados_key_api)
+analista_bi = get_dados("Analista BI",analista_bi_key_api)
+cientista_dados = get_dados("Cientista Dados",cientista_dados_key_api)
+engenheiro_dados = get_dados("Engenheiro Dados",engenheiro_dados_key_api)    
 
 df1 = pd.DataFrame(analista_dados)
 df2 = pd.DataFrame(analista_bi)
