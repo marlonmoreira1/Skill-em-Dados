afbcae9952f4722a075d724aacf2cdaa04401699
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-10-21 09:20:23 -0300

    Update etl_diario.py

--- etl_diario.py
+++ etl_diario.py
@@ -54,7 +54,7 @@ def get_dados(params):
         
     return google_jobs_results, params['next_page_token']
 
-params['q'] = "analise de dados"
+params['q'] = "análise de dados"
 params['api_key'] = analista_dados_key_api
 analista_dados, next_page_token_ad = get_dados(params)
 
@@ -64,7 +64,7 @@ params['api_key'] = analista_bi_key_api
 analista_bi, next_page_token_bi = get_dados(params)
 
 
-params['q'] = "ciencia de dados"
+params['q'] = "ciência de dados"
 params['api_key'] = cientista_dados_key_api
 params.pop('next_page_token', None)
 cientista_dados, next_page_token_cd = get_dados(params)
