e84bf4e7ce4075244268464de4d654944b175889
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-10-29 09:46:09 -0300

    Update etl_diario.py

--- etl_diario.py
+++ etl_diario.py
@@ -54,13 +54,13 @@ def get_dados(params):
         
     return pd.DataFrame(google_jobs_results)
 
-cargos = ["dados", "data", "front-end", "back-end"]
+cargos = ["analista de dados", "analista de bi", "cientista de dados", "engenheiro de dados"]
 
 api_keys = {
-    "dados": analista_dados_key_api,
-    "data": analista_bi_key_api,
-    "front-end": cientista_dados_key_api,
-    "back-end": engenheiro_dados_key_api
+    "analista de dados": analista_dados_key_api,
+    "analista de bi": analista_bi_key_api,
+    "cientista de dados": cientista_dados_key_api,
+    "engenheiro de dados": engenheiro_dados_key_api
 }
 
 dataframes = []
