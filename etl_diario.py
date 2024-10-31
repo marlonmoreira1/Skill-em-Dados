c58ef564c6eed05281b96832cd83b5b14648da9b
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-10-31 09:33:00 -0300

    Update etl_diario.py

--- etl_diario.py
+++ etl_diario.py
@@ -57,10 +57,10 @@ def get_dados(params):
 cargos = ["analista de dados", "analista de bi", "cientista de dados", "engenheiro de dados"]
 
 api_keys = {
-    "analise de dados": analista_dados_key_api,
-    "bi": analista_bi_key_api,
-    "ciencia de dados": cientista_dados_key_api,
-    "engenharia de dados": engenheiro_dados_key_api
+    "analista de dados": analista_dados_key_api,
+    "analista de bi": analista_bi_key_api,
+    "cientista de dados": cientista_dados_key_api,
+    "engenheiro de dados": engenheiro_dados_key_api
 }
 
 dataframes = []
