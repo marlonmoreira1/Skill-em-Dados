7bc77e54f2f2e7d8111b1af77c7d7b6f18bd0a9f
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2025-01-07 15:32:11 -0300

    Update backup.py

--- backup/backup.py
+++ backup/backup.py
@@ -55,13 +55,13 @@ cursor = conn.cursor()
 #dataframe = dataframe.fillna('')
 
 dataframe = dataframe[['job_id', 'unique_key', 'date', 'company_name', 'via', 'xp', 'new_title', 'cidade', 'estado', 
-           'hard_skills', 'complemento', 'soft_skills', 'graduacoes', 'metodologia_trabalho', 
+           'is_remote', 'hard_skills', 'complemento', 'soft_skills', 'graduacoes', 'metodologia_trabalho', 
            'tipo_contrato', 'cargo']]
 
 insert_stmt = '''
 INSERT INTO [dbo].[VagasDados] (
     job_id, unique_key, date, company_name, via, xp, new_title, cidade, estado,
-    hard_skills, complemento, soft_skills, graduacoes, metodologia_trabalho,
+    is_remote, hard_skills, complemento, soft_skills, graduacoes, metodologia_trabalho,
     tipo_contrato, cargo
 ) 
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
