485fa894e4b5cfbd75d32d4a358a2fdbce161f46
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2025-02-17 20:57:56 -0300

    Update etl_diario.py

--- etl_diario.py
+++ etl_diario.py
@@ -19,10 +19,8 @@ cientista_dados_key_api = os.environ["CIENTISTA_DADOS_KEY_API"]
 engenheiro_dados_key_api = os.environ["ENGENHEIRO_DADOS_KEY_API"]
 
 params = {
-      "engine": "google_jobs",
-      "location":"Brazil",      
-      "google_domain": "google.com.br",
-      "hl": "pt-br",
+      "engine": "google_jobs",            
+      "google_domain": "google.com.br",      
       "chips": "date_posted:today",
       "output": "JSON"  
     }
